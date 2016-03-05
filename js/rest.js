55cc4c15d39b5c2e33ac2f7b5a21298df08fcae6
Quentin Brault <tititesouris@laposte.net>
2016-03-05 10:17:10 +0000

    Js create user REST

--- js/rest.js
+++ js/rest.js
@@ -1,23 +1,33 @@
 var api = "http://localhost/GoDigItREST/";
 
 $(function() {
-    $("#create-user .confirm").click(function() {
-        var username = $("#create-user .username").val();
-        var email = $("#create-user .email").val();
-        var password = $("#create-user .password").val();
-        
-        $.ajax({
-            method: "POST",
-            url: api + "users.php",
-            dataType: "json",
-            data: {
-                username: username,
-                email: email,
-                password: password
-            },
-            success: function(data, status, xhr) {
-                alert(data);
-            }
-        });
+    var createUserForm = $("#create-user");
+    createUserForm.find(".confirm").click(function() {
+
+        var username = createUserForm.find(".username").val();
+        var email = createUserForm.find(".email").val();
+        var password = createUserForm.find(".password").val();
+
+        if (username && email && password) {
+
+            $.ajax({
+                method: "POST",
+                url: api + "users.php",
+                dataType: "json",
+                data: {
+                    username: username,
+                    email: email,
+                    password: password
+                },
+                success: function(data, status, xhr) {
+                    if ("error" in data) {
+                        createUserForm.find(".error").text("Error: " + data.error);
+                    }
+                    else {
+                        createUserForm.find(".success").text("Success! " + data.username);
+                    }
+                }
+            });
+        }
     });
 });
