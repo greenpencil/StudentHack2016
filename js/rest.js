1be1fe31273e8c5424f88a8ac720b44d713c2406
Yamelio <guillaume.kleinpoort@gmail.com>
2016-03-05 11:59:56 +0100

    Starting rest thingies

--- js/rest.js
+++ js/rest.js
@@ -30,4 +30,44 @@ $(function() {
             });
         }
     });
+
+    var updateUserForm = $("#update-user");
+
+    var username = "Jean-Marie";
+    var email = "jean-marie@mdr.fr";
+    var password = "NESTCEPAS";
+
+    updateUserForm.find(".username").val(username);
+    updateUserForm.find(".email").val(email);
+
+    updateUserForm.find(".confirm").click(function() {
+
+        var username = updateUserForm.find(".username").val();
+        var email = updateUserForm.find(".email").val();
+        var password = updateUserForm.find(".password").val();
+
+        if (username && email && password) {
+
+            $.ajax({
+                method: "PUT",
+                url: api + "users.php",
+                dataType: "json",
+                data: {
+                    username: username,
+                    email: email,
+                    password: password
+                },
+                success: function(data, status, xhr) {
+                    if ("error" in data) {
+                        updateUserForm.find(".error").text("Error: " + data.error);
+                    }
+                    else {
+                        updateUserForm.find(".success").text("Success! " + data.username);
+                        updateUserForm.find(".email").val(email);
+                    }
+                }
+            });
+        }
+    });
+
 });
