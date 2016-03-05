0d5b802bc36e7b3c3339a8d740257fc1ce4a0816
Katie <rstamac@gmail.com>
2016-03-05 22:55:05 +0000

    Design

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+	<meta name="theme-color" content="#795548">
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+
+<nav class="brown">
+    <div class="nav-wrapper" style="width:80%; margin:0 auto;">
+      <a href="#" class="brand-logo">go d<img src="img/shovel.png" style="-webkit-transform: rotate(180deg);margin-bottom: -9px;"/>g it</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Register or Login</a></li>
+      </ul>
+    </div>
+  </nav>
+	<h4>Welcome Katie</h4>
+	<hr/>
+	<h5>Things to do</h5>
+	<div class="row" style="margin-bottom:0px;">
+      <div class="col s12 m12 l12">
+        <div class="card-panel light-green lighten-3" style="margin: -0.5rem 0 1rem 0;">
+		<i class="fa fa-search"></i> Notification
+        </div>
+      </div>
+    </div>
+
+	<div class="row" style="margin-bottom:0px;">
+	      <div class="col s12 m12 l12">
+		<div class="card-panel blue lighten-3" style="margin: -0.5rem 0 1rem 0;">
+			<i class="fa fa-search"></i> Notification
+		</div>
+	      </div>
+	    </div>
+
+	<hr />
+	<h5>Join a hunt</h5>
+	
+	<hr/>
+	<h5>Local feed</h5>
+<iframe src="http://twubs.com/embed/godigit/?messagesPerPage=5&headerBgColor=%231c6485&headerTextColor=%23ffffff" width="350" height="600" frameborder="0"><a href="http://twubs.com/godigit">#godigit</a></iframe>
+
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+</html>
